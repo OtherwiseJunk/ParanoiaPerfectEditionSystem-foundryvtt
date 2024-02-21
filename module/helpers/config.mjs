ffcdc9eec7b5cab6f01b04d640f880e9cb0bd180
OtherwiseJunk <OtherwiseJunk@gmail.com>
2024-02-21 00:43:18 -0500

    remove unusued config

--- module/helpers/config.mjs
+++ module/helpers/config.mjs
@@ -4,20 +4,4 @@ export const PARANOIA = {};
  * The set of Ability Scores used within the sytem.
  * @type {Object}
  */
- PARANOIA.abilities = {
-  "str": "PARANOIA.AbilityStr",
-  "dex": "PARANOIA.AbilityDex",
-  "con": "PARANOIA.AbilityCon",
-  "int": "PARANOIA.AbilityInt",
-  "wis": "PARANOIA.AbilityWis",
-  "cha": "PARANOIA.AbilityCha"
-};
 
-PARANOIA.abilityAbbreviations = {
-  "str": "PARANOIA.AbilityStrAbbr",
-  "dex": "PARANOIA.AbilityDexAbbr",
-  "con": "PARANOIA.AbilityConAbbr",
-  "int": "PARANOIA.AbilityIntAbbr",
-  "wis": "PARANOIA.AbilityWisAbbr",
-  "cha": "PARANOIA.AbilityChaAbbr"
-};
