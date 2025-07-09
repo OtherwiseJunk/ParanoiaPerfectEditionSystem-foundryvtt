7c66918bcda8def43e322d21c7efef2b8c6022ca
Wren <OtherwiseJunk@gmail.com>
2025-07-09 01:18:21 +0000

    remove vitest config and update system.json

--- vitest.config.js
+++
@@ -1,8 +0,0 @@
-import { defineConfig } from 'vitest/config';
-
-export default defineConfig({
-    test: {
-        // This tells Vitest to run our setup file before executing the tests.
-        setupFiles: ['./tests/setup.js'],
-    },
-});
